ab47b1fc7ebdff7ca55583bcab0de74fffd861d9
Tom Dickman <tomdickman@catalyst-au.net>
2019-08-01 13:31:25 +1000

    Revert "Issue#7: Move not yet used settings to dev branch"
    This reverts commit 75fce70e1199f2ceab59d8a5e7ba38b622b91b94.
    
    Revert master removal of settings on dev branch

--- settings.php
+++ settings.php
@@ -29,6 +29,91 @@ if ($hassiteconfig) {
     $settings = new admin_settingpage('local_smartmedia', get_string('pluginname', 'local_smartmedia'));
     $ADMIN->add('localplugins', $settings);
 
+    // AWS Settings settings.
+    $settings->add(new admin_setting_heading('awsheader',
+            get_string('settings:aws:header', 'local_smartmedia'),
+            get_string('settings:aws:header_desc', 'local_smartmedia')));
+
+    $settings->add(new admin_setting_configtext('local_smartmedia/api_key',
+            get_string('settings:aws:key', 'local_smartmedia'),
+            get_string('settings:aws:key_help', 'local_smartmedia'),
+            ''));
+
+    $settings->add(new admin_setting_configpasswordunmask('local_smartmedia/api_secret',
+            get_string('settings:aws:secret', 'local_smartmedia'),
+            get_string('settings:aws:secret_help', 'local_smartmedia'),
+            ''));
+
+    $settings->add(new admin_setting_configtext('local_smartmedia/s3_input_bucket',
+            get_string('settings:aws:input_bucket', 'local_smartmedia'),
+            get_string('settings:aws:input_bucket_help', 'local_smartmedia'),
+            ''));
+
+    $settings->add(new admin_setting_configtext('local_smartmedia/s3_output_bucket',
+            get_string('settings:aws:output_bucket', 'local_smartmedia'),
+            get_string('settings:aws:output_bucket_help', 'local_smartmedia'),
+            ''));
+
+    $regionoptions = array(
+        'us-east-1'      => 'us-east-1 (N. Virginia)',
+        'us-east-2'      => 'us-east-2 (Ohio)',
+        'us-west-1'      => 'us-west-1 (N. California)',
+        'us-west-2'      => 'us-west-2 (Oregon)',
+        'ap-northeast-1' => 'ap-northeast-1 (Tokyo)',
+        'ap-northeast-2' => 'ap-northeast-2 (Seoul)',
+        'ap-northeast-3' => 'ap-northeast-3 (Osaka)',
+        'ap-south-1'     => 'ap-south-1 (Mumbai)',
+        'ap-southeast-1' => 'ap-southeast-1 (Singapore)',
+        'ap-southeast-2' => 'ap-southeast-2 (Sydney)',
+        'ca-central-1'   => 'ca-central-1 (Canda Central)',
+        'cn-north-1'     => 'cn-north-1 (Beijing)',
+        'cn-northwest-1' => 'cn-northwest-1 (Ningxia)',
+        'eu-central-1'   => 'eu-central-1 (Frankfurt)',
+        'eu-west-1'      => 'eu-west-1 (Ireland)',
+        'eu-west-2'      => 'eu-west-2 (London)',
+        'eu-west-3'      => 'eu-west-3 (Paris)',
+        'sa-east-1'      => 'sa-east-1 (Sao Paulo)'
+    );
+
+    $settings->add(new admin_setting_configselect('local_smartmedia/api_region',
+            get_string('settings:aws:region', 'local_smartmedia'),
+            get_string('settings:aws:region_help', 'local_smartmedia'),
+            'ap-southeast-2',
+            $regionoptions));
+
+    // Processing settings.
+    $settings->add(new admin_setting_heading('processingheader',
+        get_string('settings:processing:header', 'local_smartmedia'),
+        get_string('settings:processing:header_desc', 'local_smartmedia')));
+    $settings->add(new admin_setting_configtextarea('local_smartmedia/transcodepresets',
+        get_string('settings:processing:transcodepresets', 'local_smartmedia'),
+        get_string('settings:processing:transcodepresets_desc', 'local_smartmedia'), ''));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectlabels',
+        get_string('settings:processing:detectlabels', 'local_smartmedia'),
+        get_string('settings:processing:detectlabels_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectmoderation',
+        get_string('settings:processing:detectmoderation', 'local_smartmedia'),
+        get_string('settings:processing:detectmoderation_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectfaces',
+        get_string('settings:processing:detectfaces', 'local_smartmedia'),
+        get_string('settings:processing:detectfaces_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectpeople',
+        get_string('settings:processing:detectpeople', 'local_smartmedia'),
+        get_string('settings:processing:detectpeople_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/transcribe',
+        get_string('settings:processing:transcribe', 'local_smartmedia'),
+        get_string('settings:processing:transcribe_desc', 'local_smartmedia'), 1));
+    // TODO: figure out how to disable these settings if transcribe is disabled.
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectsentiment',
+        get_string('settings:processing:detectsentiment', 'local_smartmedia'),
+        get_string('settings:processing:detectsentiment_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectphrases',
+        get_string('settings:processing:detectphrases', 'local_smartmedia'),
+        get_string('settings:processing:detectphrases_desc', 'local_smartmedia'), 1));
+    $settings->add(new admin_setting_configcheckbox('local_smartmedia/detectentities',
+        get_string('settings:processing:detectentities', 'local_smartmedia'),
+        get_string('settings:processing:detectentities_desc', 'local_smartmedia'), 1));
+
     // FFprobe settings.
     $settings->add(new admin_setting_heading('ffprobeheader',
         get_string('settings:ffprobe:header', 'local_smartmedia'),
