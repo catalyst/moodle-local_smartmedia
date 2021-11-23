b40d6e73d4a22f17ddfa47057aa7734c4d04c137
Kevin Pham <keevan.pham@gmail.com>
2021-11-23 15:23:05 +1100

    Add missing foreign key for 'contenthash' upgrades

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_smartmedia';
-$plugin->release = 2021111000;
-$plugin->version = 2021111000;
+$plugin->release = 2021111001;
+$plugin->version = 2021111001;
 $plugin->requires = 2018051700;
 $plugin->dependencies = array(
     'local_aws' => 2020061500
